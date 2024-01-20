f3208b318819c6928c44ae6e92b9675ad476dcc1
Anton Sidorov <Anton_sid1984@mail.ru>
2024-01-20 07:44:47 +0300

    Added code to solve the task

--- DZ_sem_10.py
+++ DZ_sem_10.py
@@ -1,1 +1,13 @@
 
+import random
+import pandas as pd
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI':lst})
+data.loc[data['whoAmI'] == 'robot', 'robots_class'] = '1'
+data.loc[data['whoAmI'] != 'robot', 'robots_class'] = '0'
+data.loc[data['whoAmI'] == 'human', 'humans_class'] = '1'
+data.loc[data['whoAmI'] != 'human', 'humans_class'] = '0'
+data.head(n=20) 
+print(data)
